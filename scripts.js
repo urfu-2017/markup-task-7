fc973aff34a1394ed90a0439644128113ddfd27a
danmerus <leoghost@mail.ru>
2017-11-26 16:03:42 +0500

    v 1

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function button_click_max() {
+    document.getElementById("test").style.background = "#ffe05e";
+}
+
+function button_click_alex() {
+    document.getElementById("test").style.background = "#72c5f6";
+}
+
+function button_click_george() {
+    document.getElementById("test").style.background = "#ff8384";
+}
