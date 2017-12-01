f61ac6797972c1f33ea713edb16655f1f25be69f
danmerus <leoghost@mail.ru>
2017-12-01 12:50:57 +0500

    middle+minor stuff

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 function button_click_max() {
     document.getElementById("test").style.background = "#ffe05e";
+    document.getElementById("bck").src="podlojka-max.svg";
 }
 
 function button_click_alex() {
     document.getElementById("test").style.background = "#72c5f6";
+    document.getElementById("bck").src="podlojka-alex.svg";
 }
 
 function button_click_george() {
     document.getElementById("test").style.background = "#ff8384";
+    document.getElementById("bck").src="podlojka-george.svg";
 }
